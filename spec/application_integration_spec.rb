63a373eafe7a418779dafc73b58bd96228227937
Cheryl K <cherylkelson@icloud.com>
2019-03-03 18:28:50 +0000

    Automatically backed up by Learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -30,3 +30,13 @@ describe "GET '/' - Greeting Form" do
 
 
 end
+
+*******
+require 'spec_helper'
+ 
+describe "GET '/' - Greeting Form" do
+  it 'welcomes the user' do
+    visit '/'
+    expect(page.body).to include("Welcome!")
+  end
+end
